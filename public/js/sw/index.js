3ab396d828a40d1e2db54079e58fbd7dc4459262
Dinh Le <dinh.v.le@gmail.com>
2018-01-14 01:02:19 -0600

    hijacking request

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -2,5 +2,9 @@ self.addEventListener('fetch', function(event) {
 	// TODO: respond to all requests with an html response
 	// containing an element with class="a-winner-is-me".
 	// Ensure the Content-Type of the response is "text/html"
-  console.log(event.request);
+  event.respondWith(
+    new Response('<h1 class="a-winner-is-me">Me is winner</h1>', {
+      headers: {'Content-Type': 'text/html'}
+    })
+  );
 });
